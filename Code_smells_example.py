57c574abf2159a8b9691c1425dd1180e6158b4df
Eleodoro <velas.eleo@gmail.com>
2023-10-26 13:46:16 -0700

    Fixed Code smell #1

--- Code_smells_example.py
+++ Code_smells_example.py
@@ -1,10 +1,14 @@
 #Code Smells Example for long method
+def get_discount_rate(price):
+    if price > 100
+        return 0.9
+    else    
+        return 0.95
+
 def calculate_total_price(items):
     total = 0
     for item in items:
         if item.quantity > 0:
-            if item.price > 100:
-                total += item.price * 0.9
-            else:
-                total += item.price * 0.95
+            discount_rate = get_discount_rate(item.price)
+            total += item.price * discount_rate
     return total
